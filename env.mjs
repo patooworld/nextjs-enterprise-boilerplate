81fe363d4ebae57d5384b2babc619bd01085d56f
PATOOWORLD <info@patooworld.com>
2024-05-31 04:48:07 -0700

    Update env.mjs
    Signed-off-by: PATOOWORLD <info@patooworld.com>

--- env.mjs
+++ env.mjs
@@ -1,15 +1,15 @@
-import { createEnv } from "@t3-oss/env-nextjs"
-import { z } from "zod"
-
 export const env = createEnv({
   server: {
-    ANALYZE: z
-      .enum(["true", "false"])
-      .optional()
-      .transform((value) => value === "true"),
+    // Server variables
+    SECRET_KEY: z.string(),
   },
-  client: {},
+  client: {
+    // Client variables
+    API_URL: z.string().url(),
+  },
   runtimeEnv: {
-    ANALYZE: process.env.ANALYZE,
+    // Assign runtime variables
+    SECRET_KEY: process.env.SECRET_KEY,
+    API_URL: process.env.NEXT_PUBLIC_API_URL,
   },
 })
